5b643e6c0c9c6ceed5b4278a569028448e4dd9e4
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-08-31 23:17:26 +0100

    Added Database Interaction to the project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -55,7 +56,19 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+
+    console.log(
+      `Server is running at http://127.0.0.1:${PORT}`
+    );
+
     console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error(
+      'Error connecting to the database:',
+      error
+    );
+  }
 });
