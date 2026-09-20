d16c05732066e200d0dedb26bab3d23b14885669
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-09-20 22:05:40 +0100

    Ensure flash session saves before redirect

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,43 +1,75 @@
-import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import {
+    getAllOrganizations,
+    getOrganizationDetails,
+    createOrganization
+} from '../models/organizations.js';
 import { getProjectsByOrganizationId } from '../models/projects.js';
-import { createOrganization } from '../models/organizations.js';
 
 const showOrganizationsPage = async (req, res) => {
     const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title, organizations });
+
+    res.render('organizations', {
+        title,
+        organizations
+    });
 };
 
 const showOrganizationDetailsPage = async (req, res) => {
     const organizationId = req.params.id;
+
     const organizationDetails = await getOrganizationDetails(organizationId);
     const projects = await getProjectsByOrganizationId(organizationId);
+
     const title = 'Organization Details';
-    res.render('organization', { title, organizationDetails, projects });
+
+    res.render('organization', {
+        title,
+        organizationDetails,
+        projects
+    });
 };
 
-const processNewOrganizationForm = async (req, res) => {
-    const { name, description, contactEmail } = req.body;
-    const logoFilename = 'placeholder-logo.png';
-    const organizationId = await createOrganization(
-        name,
-        description,
-        contactEmail,
-        logoFilename
-    );
+const processNewOrganizationForm = async (req, res, next) => {
+    try {
+        const { name, description, contactEmail } = req.body;
+        const logoFilename = 'placeholder-logo.png';
 
-    req.flash('success', 'Organization added successfully!');
-    res.redirect(`/organization/${organizationId}`);
+        const organizationId = await createOrganization(
+            name,
+            description,
+            contactEmail,
+            logoFilename
+        );
+
+        req.flash(
+            'success',
+            'Organization added successfully!'
+        );
+
+        req.session.save((err) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.redirect(`/organization/${organizationId}`);
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 const showNewOrganizationForm = async (req, res) => {
     const title = 'Add New Organization';
-    res.render('new-organization', { title });
+
+    res.render('new-organization', {
+        title
+    });
 };
 
-export { 
-    showOrganizationsPage, 
-    showOrganizationDetailsPage, 
+export {
+    showOrganizationsPage,
+    showOrganizationDetailsPage,
     showNewOrganizationForm,
     processNewOrganizationForm
 };
